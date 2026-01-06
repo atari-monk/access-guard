99b317939fe5cba0ae058b8addf9d10b910e09bb
atari-monk <atari.monk1@gmail.com>
2026-01-06 11:51:59 +0100

    fix: SQLite path for Render (/tmp)

--- app/database.py
+++ app/database.py
@@ -2,7 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./accessguard.db")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:////tmp/accessguard.db"
+)
 
 engine = create_engine(
     DATABASE_URL,
